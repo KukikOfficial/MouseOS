456b57dd5ad7d7c3900b234bf626060a9a6acb5c
KukikOfficial <121604132+KukikOfficial@users.noreply.github.com>
2026-03-03 18:16:31 +0700

    Update mouse.h

--- kernel/include/mouse.h
+++ kernel/include/mouse.h
@@ -3,13 +3,10 @@
 
 #include <stdint.h>
 
-// Инициализация PS/2 контроллера для мыши
-void init_mouse_driver();
-
-// Функция отрисовки курсора (вызывается в цикле или прерывании)
-void update_mouse_cursor();
-
-// Основной обработчик прерывания (для boot.asm)
-extern "C" void mouse_handler_main();
+extern "C" {
+    void init_mouse_driver();
+    void update_mouse_cursor();
+    void mouse_handler_main();
+}
 
 #endif
