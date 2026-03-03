09f9fba541b083754d228e8d5b2377a47e4fb921
KukikOfficial <121604132+KukikOfficial@users.noreply.github.com>
2026-03-03 17:56:32 +0700

    Update font.h

--- kernel/include/font.h
+++ kernel/include/font.h
@@ -1,12 +1,6 @@
 #ifndef FONT_H
 #define FONT_H
-
 #include <stdint.h>
-
-// Массив шрифта 8x8 (базовый набор ASCII)
-extern unsigned char font8x8[128][8];
-
 void draw_char(int x, int y, char c, uint32_t color);
 void draw_string(int x, int y, const char* str, uint32_t color);
-
 #endif
